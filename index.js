c01dfc6332cab96d37f6030597e1eb56967c3b0d
Luna <lunaashere@gmail.com>
2026-07-04 16:57:29 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,97 @@
+const { REST, Routes, Client, GatewayIntentBits, Collection } = require('discord.js');
+const client = new Client({intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildModeration
+]});
+
+const fs = require("node:fs");
+const config = require('./config.json');
+
+/*
+--------------------------
+ Register Command List
+--------------------------
+*/
+
+const commands = [];
+client.commands = new Collection();
+
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+
+    // Grab a list of the files in each command "type" folder
+    const commandFiles = fs
+        .readdirSync(`./commands/${folder}`)
+        .filter((file) => file.endsWith('.js'));
+    
+    // Loop through each command file
+    for (const file of commandFiles) {
+
+        const command = require(`./commands/${folder}/${file}`);
+        commands.push(command.data.toJSON());
+        // Ensure the command has the correct properties
+        if('data' in command && 'execute' in command) {
+            // Register the command in the array for later registration
+            client.commands.set(command.data.name, command);
+        } else {
+            // If there is an error, log it.
+            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+        }
+
+    }
+
+}
+
+/*
+--------------------------
+ Event Registration
+--------------------------
+*/
+
+const eventFiles = fs
+    .readdirSync('./events')
+    .filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
+}
+
+/*
+--------------------------
+ Register Commands to Discord
+--------------------------
+*/
+const rest = new REST({ version: '10' }).setToken(config.token);
+
+(async () => {
+    try {
+
+        console.log(`Started refreshing ${commands.length} applicaiton (/) commands.`);
+
+        // Send the command to the Discord API
+        const data = await rest.put(
+            Routes.applicationCommands(config.clientid),
+            { body: commands }
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+
+    } catch (error) {
+
+        console.error(error);
+
+    }
+})();
+
+// Login to Discord
+client.login(config.token);
+
